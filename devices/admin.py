231a0e11d123d0fed0abd5823fd21a732f039e03
Cory Perkins <Link2cory@gmail.com>
2015-07-03 17:52:46 -0700

    added the devices models to be visible and editable in the admin site

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Network, Node, SensorType, ActuatorType, Sensor, Actuator, SensorData, ActuatorData
+
+admin.site.register(Network)
+admin.site.register(Node)
+admin.site.register(SensorType)
+admin.site.register(ActuatorType)
+admin.site.register(Actuator)
+admin.site.register(SensorData)
+admin.site.register(ActuatorData)
