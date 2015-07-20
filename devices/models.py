b1b4d132de764c5e771d77b5e4895807e3ae11c1
Cory Perkins <Link2cory@gmail.com>
2015-07-19 18:31:09 -0700

    added abstract models: TimingData and DisplayName for most models to extend from

--- devices/models.py
+++ devices/models.py
@@ -1,33 +1,48 @@
 from django.db import models
 
-class Network(models.Model):
-	name = models.CharField(max_length=45)
+"""Abstracts"""
+class DisplayName(models.Model):
+    name = models.CharField(max_length=45)
 
-class Node(models.Model):
-	name = models.CharField(max_length=45)
-	network = models.ForeignKey(Network)
+    def __str__(self):
+        return self.name
 
-class SensorType(models.Model):
-	name = models.CharField(max_length=45)
-	manufacturer = models.CharField(max_length=45)
-	units = models.CharField(max_length=10)
+    class Meta:
+        abstract = True 
 
-class Sensor(models.Model):
-	node  = models.ForeignKey(Node)
-	model = models.ForeignKey(SensorType)
+class TimingData(models.Model):
+    created = models.DateTimeField('created', auto_now_add=True)
+    last_modified = models.DateTimeField('last modified', auto_now=True)
 
-class ActuatorType(models.Model):
-	name = models.CharField(max_length=45)
-	manufacturer = models.CharField(max_length=45)
+    class Meta:
+        abstract = True
+"""End Abstracts"""
 
-class Actuator(models.Model):
-	node = models.ForeignKey(Node)
-	model = models.ForeignKey(ActuatorType)
+class Network(TimingData, DisplayName):
+    pass
+    
+class Node(TimingData, DisplayName):
+    network = models.ForeignKey(Network)
 
-class SensorData(models.Model):
-	value = models.IntegerField()
-	sensor = models.ForeignKey(Sensor)
+class SensorType(TimingData, DisplayName):
+    manufacturer = models.CharField(max_length=45)
+    units = models.CharField(max_length=10)
 
-class ActuatorData(models.Model):
-	value = models.IntegerField()
-	actuator = models.ForeignKey(Actuator)
+class Sensor(TimingData, DisplayName):
+    node  = models.ForeignKey(Node)
+    model = models.ForeignKey(SensorType)
+
+class ActuatorType(TimingData, DisplayName):
+    manufacturer = models.CharField(max_length=45)
+
+class Actuator(TimingData, DisplayName):
+    node = models.ForeignKey(Node)
+    model = models.ForeignKey(ActuatorType)
+
+class SensorData(TimingData):
+    value = models.IntegerField()
+    sensor = models.ForeignKey(Sensor)
+
+class ActuatorData(TimingData):
+    value = models.IntegerField()
+    actuator = models.ForeignKey(Actuator)
