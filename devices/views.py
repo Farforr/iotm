ff93a024e9fd70f4cbd17e0a3a76b1d7dde0002c
Cory Perkins <Link2cory@gmail.com>
2015-09-17 23:50:56 -0700

    added validation error handling

--- devices/views.py
+++ devices/views.py
@@ -1,25 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from .models import Network, Node
+from .models import Network, Node, Organization
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
 
 def index(request):
-    error_message = ''
+    error = ''
     if (request.POST and 'new_network_name' in request.POST):
         new_network_name = request.POST['new_network_name']
-        if (new_network_name != ""):
-            network = Network()
-            network.name = new_network_name
+
+        network = Network()
+        network.name = new_network_name
+
+        # TODO: add a real organization getter
+        network.owner = Organization.objects.get()
+
+        # if validation errors, don't save, render the same page but send
+        # the errors to the template
+        try:
+            network.full_clean()
+        except ValidationError as e:
+            error = e
+        else:
             network.save()
             return HttpResponseRedirect(reverse('devices:index'))
-        else:
-            error_message = "Please input a name for the new network."
 
     network_list = Network.objects.order_by()
+
     context = {
         'network_list': network_list,
-        'error_message': error_message
+        'error': error
     }
 
     return render(request, 'devices/index.html', context)
